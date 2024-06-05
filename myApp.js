e1e4f7bb0865ad3cb9244d450a1fab703e858c2c
RANA ABOBAKAR <95483821+ABOBAKAR-IT@users.noreply.github.com>
2024-06-05 22:43:06 +0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,14 +3,6 @@ import helmet from "helmet";
 
 const app = express();
 
-// Use Helmet!
-app.use(helmet());
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
-app.listen(3000);
 
 
 
